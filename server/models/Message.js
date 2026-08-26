7d9f876c62c65b45a386f6475f98e61be4212fbc
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-08-26 19:37:32 +0500

    work in progress

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,6 +1,3 @@
-
-module.exports = mongoose.model("Message", messageSchema);
-
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
@@ -8,7 +5,7 @@ const messageSchema = new mongoose.Schema(
     from: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     text: { type: String, required: true, trim: true, maxlength: 2000 },
-    read: { type: Boolean, default: false },   //i have  added "read"field.
+    read: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
