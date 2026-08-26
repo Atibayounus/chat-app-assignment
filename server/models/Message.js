6df08c83c32275535f6d8e4df0b3a702f434effb
Atiba Dee 💥 <atiba.dar12345@gmail.com>
2026-08-26 18:45:02 +0500

    added read field

--- server/models/Message.js
+++ server/models/Message.js
@@ -1,14 +1,14 @@
+
+module.exports = mongoose.model("Message", messageSchema);
+
 const mongoose = require("mongoose");
 
-// TODO (student):
-// The unread count feature needs one more field here, for example "read".
-// Think: what should its value be when a message is first created?
 const messageSchema = new mongoose.Schema(
   {
     from: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     text: { type: String, required: true, trim: true, maxlength: 2000 },
-    // read: ...
+    read: { type: Boolean, default: false },   //added "read"field.
   },
   { timestamps: true }
 );
